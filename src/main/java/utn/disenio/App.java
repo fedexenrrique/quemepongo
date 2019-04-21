fe21ec15836c40081f1e0c593b6eca1cd62647f3
fedee <Federico A. Enrrique>
2019-04-21 17:56:18 -0300

    Arrancando con la funcionalidad. Me falta todavía profe!

--- src/main/java/utn/disenio/App.java
+++ src/main/java/utn/disenio/App.java
@@ -1,9 +1,25 @@
 package utn.disenio;
 
+import java.util.Scanner;
+
+import utn.disenio.clima.Localization;
+import utn.disenio.clima.WeatherToday;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // INICIALIZACIONES
+    	Localization loc     = new Localization();
+    	WeatherToday weather = new WeatherToday();
+    	Scanner s = new Scanner(System.in);
+    	
+    	System.out.println( "Indique cuál es el clima de hoy en Buenos Aires." );
+        System.out.println( "  1 - Frío \n  2 - Fresco \n  3 - Cálido \n  4 - Caluroso " );
+        System.out.print("Ingrese opción: ");
+        Integer option = s.nextInt();
+        
+        System.out.println(option);
+        
     }
 }
--- src/main/java/utn/disenio/clima/WeatherToday.java
+++ src/main/java/utn/disenio/clima/WeatherToday.java
@@ -3,5 +3,12 @@ package utn.disenio.clima;
 public class WeatherToday {
 	
 	public enum sensacion {FRIO, FRESCO, CALIDO, CALUROSO};
+	public enum variantes {DESPEJADO, NUBLADO, LLUEVE,TORMENTA}
+	
+	@Override
+	public String toString() {
+		return "WeatherToday [getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ super.toString() + "]";
+	};
 
 }
